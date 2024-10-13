dad0672ffe7ad45b71e45d64435d4474914ad1b6
Chopragit <139036539+Chopragit@users.noreply.github.com>
2024-10-13 23:37:57 +0530

    Add files via upload

---
+++ Day-13.py
@@ -0,0 +1,11 @@
+# Read the number of test cases
+T = int(input(" "))
+
+for _ in range(T):
+    # Read the amount of pulp in kg for each test case
+    N = int(input())
+    # Calculate the number of notebooks
+    notebooks = N * 10
+    # Output the result
+    print(notebooks)
+
